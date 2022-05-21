c7b1a93c27ed61d407d172aee1470bbbb0397ab0
“imop” <woolan999@gmail.com>
2022-05-21 14:15:19 +0800

    project initialized

---
+++ app/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>App 组件</h1>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style></style>
---
+++ app/vue.config.js
@@ -0,0 +1,9 @@
+/* const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true
+}) */
+
+module.exports = {
+  // 关闭 eslint 校验工具
+  lintOnSave: false
+}
